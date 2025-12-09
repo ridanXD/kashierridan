feb0562656fc87aee5126bb0cddcc11b35c15f40
ridanXD <rizkyaramdancringe@gmail.com>
2025-12-09 20:50:38 +0700

    Lab day 2

--- composeApp/src/jvmMain/kotlin/com/chevalierlabsas/kashierridan/main.kt
+++ composeApp/src/jvmMain/kotlin/com/chevalierlabsas/kashierridan/main.kt
@@ -2,6 +2,7 @@ package com.chevalierlabsas.kashierridan
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.chevalierlabsas.kashierridan.core.App
 
 fun main() = application {
     Window(
